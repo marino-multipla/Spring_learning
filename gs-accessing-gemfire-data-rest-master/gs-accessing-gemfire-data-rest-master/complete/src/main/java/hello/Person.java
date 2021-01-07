3cb910f71ad7afe39434fcf8054ca00b2bfcd436
Giacomo <75014126+marino-multipla@users.noreply.github.com>
2021-01-07 01:08:57 +0100

    Test Gemfire data and rest in Spring

---
+++ gs-accessing-gemfire-data-rest-master/gs-accessing-gemfire-data-rest-master/complete/src/main/java/hello/Person.java
@@ -0,0 +1,28 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.gemfire.mapping.annotation.Region;
+
+import lombok.Data;
+
+@Data
+@Region("People")
+public class Person {
+
+	private static AtomicLong COUNTER = new AtomicLong(0L);
+
+	@Id
+	private Long id;
+
+	private String firstName;
+	private String lastName;
+	private String multipla;
+
+	@PersistenceConstructor
+	public Person() {
+		this.id = COUNTER.incrementAndGet();
+	}
+}
